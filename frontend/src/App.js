ec2d691a3f530d13dfd9b5712d288d8f23984390
Yury Ivashchanka <yury.ivashchanka@yahoo.com>
2021-02-10 20:29:40 +0300

    finished merging

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 import EventForm from "./components/eventForm/EventForm";
 import LogInForm from "./components/logInForm/LogInForm";
 import RegisterForm from "./components/registerForm/RegisterForm";
-=======
 import { Route, Switch } from "react-router-dom";
 import LandingPage from "./components/landingPage/LandingPage";
 import Login from './components/login/Login';
@@ -11,17 +9,14 @@ import MapSection from './components/mapSection/MapSection';
 import MyEvents from './components/myEvents/MyEvents';
 import CreateEvent from './components/createEvent/CreateEvent';
 import JoinEvent from './components/joinEvent/JoinEvent';
->>>>>>> 40ab698b30f3061b431c1b2d42593fb2e8d51f21
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <h1>Yo</h1>
       <RegisterForm/>
       <LogInForm />
       <EventForm />
-=======
       <Switch>
         <Route exact path="/" render={(props) => <LandingPage {...props} />} />
         <Route path="/login" render={(props) => <Login {...props} />} />
@@ -34,7 +29,6 @@ function App() {
 
       <LandingPage />     
 
->>>>>>> 40ab698b30f3061b431c1b2d42593fb2e8d51f21
     </div>
   );
 }

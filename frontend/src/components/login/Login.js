f5d270993d77d937141304c1571fcfbbfd52992f
Befa222 <72620186+Befa222@users.noreply.github.com>
2021-02-10 19:12:56 +0000

    put each form on their own pages

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import LogInForm from '../logInForm/LogInForm'
 
 export default function Login() {
     return (
         <div>
-            
+            <LogInForm />
         </div>
     )
 }

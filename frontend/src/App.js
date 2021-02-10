54fe70c515c15c585770455826b3da610a9d8401
IoanLT <ioan.tranole@gmail.com>
2021-02-10 15:57:56 +0000

    Created routes and basic structure of the project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
+import { Route, Switch } from "react-router-dom";
+import LandingPage from "./components/landingPage/LandingPage";
 
 function App() {
   return (
     <div className="App">
-      <h1>Yo</h1>
+      <Switch>
+        <Route exact path="/" render={(props) => <LandingPage {...props} />} />
+        <Route path="/login" render={(props) => <Login {...props} />} />
+        <Route path="/register" render={(props) => <Register {...props} />} />
+        <Route path="/mapSection" render={(props) => <MapSection {...props} />} />        
+        <Route path="/myEvents" render={(props) => <MyEvents {...props} />} />
+        <Route path="/createEvent" render={(props) => <CreateEvent {...props} />} />
+        <Route path="/joinEvent" render={(props) => <JoinEvent {...props} />} />        
+      </Switch>
+
+      <LandingPage />
     </div>
   );
 }

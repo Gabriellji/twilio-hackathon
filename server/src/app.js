6a0d2dc3bbba16377daea33d37ae97197b276c50
Gabriellji <gabriellji@yahoo.com>
2021-02-10 20:03:11 +0300

    feat: tlogin/register tested and work

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-// const userRouter = require('./resources/user/user.router')
-// const authRouter = require('./resources/user/auth.router')
+const userRouter = require('./resources/user/user.router')
+const authRouter = require('./resources/user/auth.router')
 
 app.use(express.json())
 
@@ -14,7 +14,7 @@ app.use('/', (req, res, next) => {
     next()
 })
 
-// app.use('/user', userRouter)
-// app.use('/auth', authRouter)
+app.use('/register', userRouter)
+app.use('/auth', authRouter)
 
 module.exports = app

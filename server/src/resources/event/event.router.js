93d2946c95d44c7a99775250b7cfbd8c2549affa
Gabriellji <gabriellji@yahoo.com>
2021-02-11 00:13:47 +0300

    fix: bugs to connect event with user

--- server/src/resources/event/event.router.js
+++ server/src/resources/event/event.router.js
@@ -62,25 +62,38 @@ router.post(
             min: 10,
         }),
         check('time', 'Time is required').not().isEmpty(),
+        check('event_name', 'Title is required').isLength({
+            min: 4,
+        }),
     ],
     async (req, res) => {
         const errors = validationResult(req)
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() })
         }
-        console.log(req.user)
-        const eventFields = {
+        // console.log(req.user)
+        // const eventFields = {
+        //         user: req.user.id,
+        //         name: req.user.info[0],
+        //     ...req.body,
+        // }
+        try {
+            // console.log(req.user)
+            const event = await Event.create({
                 user: req.user.id,
                 name: req.user.info[0],
-            ...req.body,
-        }
-        try {
-            // Using upsert option (creates new doc if no match is found):
-            const event = await Event.findOneAndUpdate(
-                { user: req.user.id },
-                { $set: eventFields },
-                { new: true, upsert: true, setDefaultsOnInsert: true }
-            )
+                ...req.body
+            })
+
+            const user = await User.findOne({
+                _id: req.user.id
+            })
+
+            console.log(req.user.id)
+
+            user['user_events'].push(event)
+
+            await user.save()
 
             res.status(201).json(event)
         } catch (err) {

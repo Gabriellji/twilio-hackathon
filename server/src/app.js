d9cf8a7af108f00ece5562c4e2a49beda0cd729f
Gabriellji <gabriellji@yahoo.com>
2021-02-12 04:01:43 +0300

    make it better

--- server/src/app.js
+++ server/src/app.js
@@ -27,4 +27,14 @@ app.use('/auth', authRouter)
 app.use('/event', eventRouter)
 // app.use('/', messageRouter);
 
+
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('../frontend/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
+
 module.exports = app

e5e290a60737f98245f80294850288b28e314a5b
Gabriellji <gabriellji@yahoo.com>
2021-02-12 14:33:22 +0300

    fix: bugs

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.use('/event', eventRouter)
 
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('../frontend/build'));
+  app.use(express.static('frontend/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));

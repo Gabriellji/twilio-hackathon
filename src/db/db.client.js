1e0df1be7eaeb926ad7260cb32f6e4d8a1cad084
Gabriellji <gabriellji@yahoo.com>
2021-02-12 05:34:39 +0300

    fix: bugs

--- src/db/db.client.js
+++ src/db/db.client.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-const chalk = require('chalk')
 
 const connect = runServer => {
   mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
@@ -13,7 +12,7 @@ const connect = runServer => {
   db.on('error', console.error.bind(console, 'connection error'))
   db.once('open', () => {
     // we are connected
-    console.log(chalk.blue('we are connected'))
+    console.log('we are connected')
     runServer()
   })
 }
